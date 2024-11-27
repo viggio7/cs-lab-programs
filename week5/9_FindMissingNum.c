2a217e42111ccc5aae955194903781bc03ea0143
Vigneshwar T <vignesht2k06@gmail.com>
2024-11-27 07:41:34 +0530

    Update 9_FindMissingNum.c
    logic correction

--- week5/9_FindMissingNum.c
+++ week5/9_FindMissingNum.c
@@ -3,19 +3,22 @@
 int main(){
 	int i,j,t;
 	int a[7]={6,5,3,1,2,7,5};
-	for(i=0;i<7;i++){
-		for(j=0;j<6-i;j++){
-			if(a[j+1]<a[j]){
+	for(i=0;i<7;i++)
+	{
+		for(j=0;j<6-i;j++)
+		{
+			if(a[j+1]<a[j])
+			{
 				t=a[j+1];
 				a[j+1]=a[j];
 				a[j]=t;
 			}
 		}
 	}
-	for(i=0;i<7;i++){
-		if(a[i]!=(i+1)){
-			printf("%d",a[i]);
-		}
+	for(i=0;i<7;i++)
+	{
+		if(a[i]!=(i+1))
+			printf("%d",i+1);
 	}
 	return 0;
 }
