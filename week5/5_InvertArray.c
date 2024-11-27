22018fe871b759a9ed7216f659ab9e83b220e8c6
Vigneshwar T <vignesht2k06@gmail.com>
2024-11-27 07:12:37 +0530

    Update 5_InvertArray.c
    added return 0

--- week5/5_InvertArray.c
+++ week5/5_InvertArray.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 	int i,t;
 	int a[5]={6,5,3,1,2};
-	for(i=0;i<=2;i++){
+	for(i=0;i<=2;i++)
+	{
 		t=a[4-i];
 		a[4-i]=a[i];
 		a[i]=t;
 	}
-	for(i=0;i<5;i++){
+	for(i=0;i<5;i++)
+	{
 		printf("%d",a[i]);
 	}
+	return 0;
 }
